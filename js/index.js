a4a22d5db8bd751ce94d94cd664e13e050bca33f
Karen Murgana <karenmurgana@gmail.com>
2023-08-18 04:45:24 -0300

    ADD: index.html, index.js, index.css. TODO: arreglar carrousel, refactorizar CSS.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/** Este método sirve para duplicar los elementos del carrousel, así 
+se pueden ver varios elementos del mismo a la vez */
+let items = document.querySelectorAll(".carousel .carousel-item");
+
+items.forEach((el) => {
+  const minPerSlide = 3;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
